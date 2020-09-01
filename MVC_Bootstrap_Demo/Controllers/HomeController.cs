e4d4450a3558b9d77d91ce9a7edad44456f655f9
Alex Reich <alex@alexreich.com>
2020-08-31 20:26:27 -0700

    Initial Commit

---
+++ MVC_Bootstrap_Demo/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MVC_Bootstrap_Demo.Models;
+
+namespace MVC_Bootstrap_Demo.Controllers
+{
+  public class HomeController : Controller
+  {
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+      _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+      List<Invoice> invoices = new List<Invoice>();
+      invoices.Add(new Invoice() { InvoiceNumber = "234239", InvoiceDate = new DateTime(18, 10, 30), DueDate = new DateTime(18, 10, 30), Total = 100, AmountDue = 100, Terms = "Discount of $100.00 if paid by 5/2/2020", Description = "Lorem impsum Csharp Developero", PONumber = "239482", Active = false });
+      invoices.Add(new Invoice() { InvoiceNumber = "234239", InvoiceDate = new DateTime(18, 10, 30), DueDate = new DateTime(18, 10, 30), Total = 100, AmountDue = 100, Terms = "Discount of $100.00 if paid by 5/2/2020", Description = "Lorem impsum Csharp Developero", PONumber = "239567", Active = false });
+      invoices.Add(new Invoice() { InvoiceNumber = "234239", InvoiceDate = new DateTime(18, 10, 30), DueDate = new DateTime(18, 10, 30), Total = 100, AmountDue = 100, Terms = "Discount of $100.00 if paid by 5/2/2020", Description = "Lorem impsum Csharp Developero", PONumber = "239454", Active = false });
+      invoices.Add(new Invoice() { InvoiceNumber = "234239", InvoiceDate = new DateTime(18, 10, 30), DueDate = new DateTime(18, 10, 30), Total = 100, AmountDue = 100, Terms = "Discount of $100.00 if paid by 5/2/2020", Description = "Lorem impsum Csharp Developero", PONumber = "239454", Active = true });
+      invoices.Add(new Invoice() { InvoiceNumber = "234239", InvoiceDate = new DateTime(18, 10, 30), DueDate = new DateTime(18, 10, 30), Total = 100, AmountDue = 100, Terms = "Discount of $100.00 if paid by 5/2/2020", Description = "Lorem impsum Csharp Developero", PONumber = "239345", Active = true });
+
+      InvoiceViewModel ivm = new InvoiceViewModel();
+      ivm.Invoices = invoices;
+      ivm.OpenInvoices = invoices.Count(w => w.Active);
+      ivm.TotalDue = invoices.Where(w => !w.Active).Sum(w => w.AmountDue);
+
+      return View(ivm);
+    }
+
+    public IActionResult Privacy()
+    {
+      return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+      return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+  }
+}
---
+++ MVC_Bootstrap_Demo/Models/Invoice.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC_Bootstrap_Demo.Models
+{
+  public class InvoiceViewModel
+  {
+    public List<Invoice> Invoices { get; set; }
+    public decimal TotalDue { get; set; }
+    public int OpenInvoices { get; set; }
+    public int OpenSalesOrders { get; set; }
+    public decimal TotalCredits { get; set; }
+    public bool AutoPay { get; set; }
+  }
+
+  public class Invoice
+  {
+    public string InvoiceNumber { get; set; }
+    [DisplayFormat(DataFormatString = "{0:MM/dd/yy}")]
+    public DateTime InvoiceDate { get; set; }
+    [DisplayFormat(DataFormatString = "{0:MM/dd/yy}")]
+    public DateTime DueDate { get; set; }
+    public Decimal Total { get; set; }
+    public Decimal AmountDue { get; set; }
+    public string Terms { get; set; }
+    public string Description { get; set; }
+    public string PONumber { get; set; }
+    public bool Active { get; set; }
+  }
+}
